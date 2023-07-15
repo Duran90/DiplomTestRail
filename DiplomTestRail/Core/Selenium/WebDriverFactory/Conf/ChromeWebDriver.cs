f161c28dba641bf9cc29d1ea481a6602d91e4b09
Игорь Карданов <Duran.kar90@gmail.com>
2023-07-15 18:28:55 +0300

    Add WebDriverFactory

---
+++ DiplomTestRail/Core/Selenium/WebDriverFactory/Conf/ChromeWebDriver.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomTestRail.Core.Selenium.WebDriverFactory.Conf
+{
+    public class ChromeWebDriver
+    {
+        public WebDriver newDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--diable-gpu");
+            options.AddArgument("incognito");
+            options.AddArgument("--start-maximized");
+            return new ChromeDriver(options);
+        }
+    }
+}
