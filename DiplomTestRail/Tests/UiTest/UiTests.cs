10f7c56c7b9a60fe9f12b5153530adc80ecff834
Игорь Карданов <Duran.kar90@gmail.com>
2023-07-17 17:04:46 +0300

    change TestUI

--- DiplomTestRail/Tests/UiTest/UiTests.cs
+++ DiplomTestRail/Tests/UiTest/UiTests.cs
@@ -1,14 +1,18 @@
+using DiplomTestRail.Core.Selenium.WebDriverFactory;
 using OpenQA.Selenium;
 
 namespace DiplomTestRail.Tests.UiTest
 {
     public class UiPositiveTests
     {
+
+        private string browser = "";//TODO
         private WebDriver driver;
+        
         [SetUp]
         public void Setup()
         {
-            this.driver = new DriverFa
+            this.driver = new DriverFactory().getDriver(browser);
         }
 
         [Test]
